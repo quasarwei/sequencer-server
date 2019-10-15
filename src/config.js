84df93bfa71c79a86be9c9669ba83df2aa27dbd4
Quasar Wei <quasar.wei@outlook.com>
2019-10-15 08:05:02 -0700

    change to DATABASE_URL deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/sequencer',
-  DB_TEST_URL: process.env.DB_TEST_URL || 'postgresql://postgres@localhost/sequencer-test',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/sequencer',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/sequencer-test',
   JWT_SECRET: process.env.JWT_SECRET || 'my-jwt-secret-only'
 };
